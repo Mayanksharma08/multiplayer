196343a8020ae94bb06881ccd7e0e23afba293a2
Mayank Sharma <mayank.sharma049@gmail.com>
2020-05-22 01:53:46 +0530

    sixth commit

--- server.js
+++ server.js
@@ -110,10 +110,11 @@ function updateGame(room) {
 }
 
 // USE THIS ON GLITCH
-// http.listen(process.env.PORT, () => {
-//  console.log("Your app is listening on port " + process.env.PORT);
-// });
-
+ http.listen(process.env.PORT, () => {
+  console.log("Your app is listening on port " + process.env.PORT);
+ });
+/*
 http.listen(3000, () => {
 	console.log('Your app is listening on port ' + 3000);
 });
+*/
